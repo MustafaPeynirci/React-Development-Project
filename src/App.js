5fb117a1b025470585119e9e285ce0008dddbafe
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2023-05-08 14:29:32 +0300

    useEffect useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 
 function App() {
+  const [data, setData] = useState(0);
+
+  useEffect(() => {
+    console.log('useEffect');
+  }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setData(data + 1)}>Click me +</button>
+      <button onClick={() => setData(data - 1)}>Click me -</button>
+      <p>
+        {data}
+      </p>
     </div>
   );
 }
